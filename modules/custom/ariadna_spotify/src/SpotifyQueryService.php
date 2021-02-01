6cc7ef77c9c3a39bf4b1eec097feb35c2bb9a4ef
Julian David Guerrero <jdgp77@gmail.com>
2021-01-31 19:46:28 -0500

    Crea servicio base para consultas

---
+++ modules/custom/ariadna_spotify/src/SpotifyQueryService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\ariadna_spotify;
+
+/**
+ * Class SpotifyQueryService.
+ */
+class SpotifyQueryService {
+
+  /**
+   * Constructs a new SpotifyQueryService object.
+   */
+  public function __construct() {
+
+  }
+
+}
